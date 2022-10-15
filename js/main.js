4758226cb947eaffd5fb8e790993952b4dec7d17
Fabricio Henrique da Silva <fabricionhsilva@gmail.com>
2022-10-14 21:05:40 -0300

    applying best practices of organization to project

--- js/main.js
+++ js/main.js
@@ -4,27 +4,27 @@ const uriPokemon = 'https://pokeapi.co/api/v2/pokemon/';
 const mainContentHtml = document.querySelector('.content');
 
 async function getPokemonData() {
-    for (let i = 1; i < 151; i++) {
+    for (let i = 1; i <= 151; i++) {
         let data = await fetch(uriPokemon + i)
             .then(res => res.json())
-            .then(pokemon => createPokemonCard(pokemon))
+            .then(pokemon => createPokemonCard(pokemon));
     }
 }
 
 function createPokemonCard(pokemonData) {
     let types = '';
     for (let i = 0; i < pokemonData.types.length; i++) {
-        types += `<li class="content__pokemon-type-item ${pokemonData.types[i].type.name}">${pokemonData.types[i].type.name}</li>`;
+        types += `<li class="pokemon-card__type ${pokemonData.types[i].type.name}">${pokemonData.types[i].type.name}</li>`;
     }
 
     mainContentHtml.innerHTML += `
-        <article class="content__pokemon-card">
-            <div class="content__pokemon_id" data-id="${pokemonData.id}">${pokemonData.id}</div>
+        <article class="pokemon-card">
+            <div class="pokemon-card__id" data-id="${pokemonData.id}">#${pokemonData.id}</div>
             <img src="${pokemonData.sprites.other["official-artwork"]["front_default"]}" 
             alt=""
-            class="content__pokemon-picture">
-            <h2 class="content__pokemon-name">${pokemonData.name}</h2>
-            <ul class="content__pokemon-types">${types}
+            class="pokemon-card__picture">
+            <h2 class="pokemon-card__name">${pokemonData.name}</h2>
+            <ul class="pokemon-card__types">${types}
             </ul>
         </article>`;
 }
