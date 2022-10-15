edc43784f348665094f3d098af58d67f1b003c2f
Fabricio Henrique da Silva <fabricionhsilva@gmail.com>
2022-10-15 10:12:29 -0300

    added filter pokemon type by aside buttons

--- js/main.js
+++ js/main.js
@@ -4,7 +4,8 @@ const uriPokemon = 'https://pokeapi.co/api/v2/pokemon/';
 const uriPokemonTypes = 'https://pokeapi.co/api/v2/type/';
 const mainContentHtml = document.querySelector('.content');
 
-let types = [];
+const types = [];
+const pokemons = [];
 
 async function getPokemonTypes(){
     for(let i = 1; i <= 18; i++){
@@ -18,28 +19,63 @@ async function getPokemonTypes(){
 
 async function getPokemonData() {
     for (let i = 1; i <= 151; i++) {
-        let data = await fetch(uriPokemon + i)
-            .then(res => res.json())
-            .then(pokemon => createPokemonCard(pokemon));
+        let res = await fetch(uriPokemon + i)
+        let pokemon = await res.json();
+
+        let card = createPokemonCard(pokemon);
+        renderCard(card);
+        pokemons.push(pokemon);
     }
 }
 
-function createPokemonCard(pokemonData) {
+function createPokemonCard(pokemon) {
     let types = '';
-    for (let i = 0; i < pokemonData.types.length; i++) {
-        types += `<li class="pokemon-card__type ${pokemonData.types[i].type.name}">${pokemonData.types[i].type.name}</li>`;
+    let card = '';
+
+    for (let i = 0; i < pokemon.types.length; i++) {
+        let type = pokemon.types[i].type.name;
+        types += `<li class="pokemon-card__type ${type}" data-type="${type}">${type}</li>`;
     }
 
-    mainContentHtml.innerHTML += `
+    card = `
         <article class="pokemon-card">
-            <div class="pokemon-card__id" data-id="${pokemonData.id}">#${pokemonData.id}</div>
-            <img src="${pokemonData.sprites.other["official-artwork"]["front_default"]}" 
+            <div class="pokemon-card__id" data-id="${pokemon.id}">#${pokemon.id}</div>
+            <img src="${pokemon.sprites.other["official-artwork"]["front_default"]}" 
             alt=""
             class="pokemon-card__picture">
-            <h2 class="pokemon-card__name">${pokemonData.name}</h2>
+            <h2 class="pokemon-card__name">${pokemon.name}</h2>
             <ul class="pokemon-card__types">${types}
             </ul>
         </article>`;
+
+    return card;
+}
+
+function renderCard(card){
+    mainContentHtml.innerHTML += `${card}`;
+}
+
+function filterPokemonCard(){
+    mainContentHtml.innerHTML = '';
+    
+    let type = this.getAttribute('data-type');
+
+    let filteredPokemons = pokemons.filter(pokemon => isPokemonType(pokemon.types, type));
+
+    filteredPokemons.forEach(pokemon => { 
+        let card = createPokemonCard(pokemon);
+        renderCard(card);
+    });
+}
+
+function isPokemonType(pokemonTypes, filter) {
+    let response = false;
+    pokemonTypes.forEach(index => {
+        if(index.type.name == filter){
+            response = true;
+        }
+    });
+    return response;
 }
 
 function createBadgesTypes(pokemonTypes) {
@@ -47,10 +83,14 @@ function createBadgesTypes(pokemonTypes) {
     badgesElement.innerHTML = '';
 
     pokemonTypes.forEach( type => {
-        badgesElement.innerHTML += `
-            <div class="badges__type ${type.name}" data-type="${type.name}">
-                ${type.name}
-            </div>`;
+        const div = document.createElement('div');
+             
+        div.classList.add('badges__type', type.name);
+        div.setAttribute('data-type', type.name);
+        div.innerText = type.name;
+        div.addEventListener('click', filterPokemonCard);
+
+        badgesElement.appendChild(div);
     });
 }
 
